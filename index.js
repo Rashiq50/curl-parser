0333fc9678ad9089313f3459c939c02d3872e71a
Rashiq Rahman <rrahman@pioneeraitech.com>
2026-03-02 13:42:45 +0600

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const __dirname = dirname(__filename);
 const app = express();
 // need to use cors to allow call from any origin
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
